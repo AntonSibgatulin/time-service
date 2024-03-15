1aad24123a0a0795defa825018c8a8c602778d42
anton <89879639755a@gmail.com>
2024-03-15 16:05:02 +0300

    ДТО получения результата

--- src/main/java/com/example/timeservice/dto/DateTimeDto.java
+++ src/main/java/com/example/timeservice/dto/DateTimeDto.java
@@ -1,22 +1,42 @@
 package com.example.timeservice.dto;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.AllArgsConstructor;
+import lombok.Data;
 import lombok.NoArgsConstructor;
 
 import java.util.Date;
 
+@Data
 @AllArgsConstructor
 @NoArgsConstructor
 public class DateTimeDto {
+    @Schema(description = "Часовой пояс сервера")
     private String timeZone;
-    private String year;
-    private String month;
-    private String day;
-    private String hour;
-    private String minutes;
-    private String seconds;
 
+    @Schema(description = "Год текущего времени")
+    private int year;
+
+    @Schema(description = "Месяц текущего времени")
+    private int month;
+
+    @Schema(description = "День текущего времени")
+    private int day;
+
+    @Schema(description = "Час текущего времени")
+    private int hour;
+
+    @Schema(description = "Минуты текущего времени")
+    private int minutes;
+
+    @Schema(description = "Секунды текущего времени")
+    private int seconds;
+
+    @Schema(description = "Объект типа Date, представляющий текущее время")
     private Date date;
+
+    @Schema(description = "Строковое представление текущего времени")
     private String dateString;
+
 
 }
