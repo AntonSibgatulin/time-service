49b6193dee2a3006eff1e50e3a378873dd152631
anton <89879639755a@gmail.com>
2024-03-15 16:06:10 +0300

    Инициализация интерфейсов

--- src/main/java/com/example/timeservice/config/mapper/MapperConfig.java
+++ src/main/java/com/example/timeservice/config/mapper/MapperConfig.java
@@ -1,3 +1,16 @@
 package com.example.timeservice.config.mapper;
 
+import com.example.timeservice.mapper.DateTimeMapper;
+import com.example.timeservice.mapper.model.DateTimeMapperImpl;
+import org.springframework.context.annotation.Bean;
+import org.springframework.stereotype.Component;
+
+@Component
 public class MapperConfig {
+
+    // инициализация di DateTime mapper singleton
+    @Bean
+    public DateTimeMapper DateTime() {
+        return new DateTimeMapperImpl();
+    }
+}
