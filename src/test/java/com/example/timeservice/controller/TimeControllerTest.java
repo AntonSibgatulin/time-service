a9fe66b4421a29875fadc46039dd975575875a4f
anton <89879639755a@gmail.com>
2024-03-15 16:07:20 +0300

    Test time controller

--- src/test/java/com/example/timeservice/controller/TimeControllerTest.java
+++ src/test/java/com/example/timeservice/controller/TimeControllerTest.java
@@ -1,2 +1,45 @@
-package com.example.timeservice.controller;public class TimeControllerTest {
+package com.example.timeservice.controller;
+
+import com.example.timeservice.dto.DateTimeDto;
+import com.example.timeservice.service.TimeService;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+
+import java.time.OffsetDateTime;
+import java.util.Date;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.when;
+
+public class TimeControllerTest {
+
+    @Mock
+    private TimeService timeService;
+
+    private TimeController timeController;
+
+    @BeforeEach
+    void setup() {
+        MockitoAnnotations.initMocks(this);
+        timeController = new TimeController(timeService);
+    }
+
+    @Test
+    void getTime_ReturnsOkResponseWithDateTimeDto() {
+        OffsetDateTime currentTime = OffsetDateTime.now();
+        DateTimeDto dateTimeDto = new DateTimeDto("UTC", currentTime.getYear(), currentTime.getMonthValue(),
+                currentTime.getDayOfMonth(), currentTime.getHour(), currentTime.getMinute(), currentTime.getSecond(), Date.from(currentTime.toInstant()),
+                currentTime.toString());
+
+        when(timeService.getTimeNow()).thenReturn(dateTimeDto);
+
+        ResponseEntity<DateTimeDto> response = timeController.getTime();
+
+        assertEquals(HttpStatus.OK, response.getStatusCode());
+        assertEquals(dateTimeDto, response.getBody());
+    }
 }
