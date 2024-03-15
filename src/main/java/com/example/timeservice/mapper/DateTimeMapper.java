f9e0dc1eca890b04bfdb08968ea23e90ed2acb52
anton <89879639755a@gmail.com>
2024-03-15 16:05:30 +0300

    интерфейс для скрытия реализации маппера ДТО времени

--- src/main/java/com/example/timeservice/mapper/DateTimeMapper.java
+++ src/main/java/com/example/timeservice/mapper/DateTimeMapper.java
@@ -1,7 +1,11 @@
 package com.example.timeservice.mapper;
 
-import com.example.timeservice.dto.DateTImeDto;
+import com.example.timeservice.dto.DateTimeDto;
 
-public interface DateTime {
-    DateTImeDto fromDate();
+import java.time.LocalDateTime;
+import java.time.OffsetDateTime;
+import java.time.ZoneId;
+
+public interface DateTimeMapper {
+    DateTimeDto fromDate(OffsetDateTime currentTime, ZoneId zoneId, String timeString);
 }
