d3dd11a9b74917a7e5a32ee3139b56e7a19908fb
anton <89879639755a@gmail.com>
2024-03-15 16:07:37 +0300

    Time service with hidden functional

--- src/main/java/com/example/timeservice/service/TimeService.java
+++ src/main/java/com/example/timeservice/service/TimeService.java
@@ -1,2 +1,27 @@
-package com.example.timeservice.service;public class TimeService {
+package com.example.timeservice.service;
+
+import com.example.timeservice.dto.DateTimeDto;
+import com.example.timeservice.mapper.DateTimeMapper;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.time.LocalDateTime;
+import java.time.OffsetDateTime;
+import java.time.ZoneId;
+import java.time.format.DateTimeFormatter;
+
+@RequiredArgsConstructor
+@Service
+public class TimeService {
+    private final DateTimeMapper dateTimeMapper;
+
+    public DateTimeDto getTimeNow() {
+        OffsetDateTime currentTime = OffsetDateTime.now();
+        ZoneId zoneId = ZoneId.systemDefault();
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss ZZZZZ");
+        String timeString = currentTime.format(formatter) + " (" + zoneId.toString() + ")";
+        return dateTimeMapper.fromDate(currentTime, zoneId, timeString);
+    }
+
+
 }
