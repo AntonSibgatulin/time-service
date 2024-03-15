294fca2f40b1e7e31d8bc7fd83f76c55e06e587f
anton <89879639755a@gmail.com>
2024-03-15 16:07:02 +0300

    time controller added endpoint time

--- src/main/java/com/example/timeservice/controller/TimeController.java
+++ src/main/java/com/example/timeservice/controller/TimeController.java
@@ -1,4 +1,32 @@
 package com.example.timeservice.controller;
 
+
+import com.example.timeservice.dto.DateTimeDto;
+import com.example.timeservice.service.TimeService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.time.LocalDateTime;
+import java.time.ZoneId;
+import java.time.format.DateTimeFormatter;
+
+@RequiredArgsConstructor
+@RestController
+@RequestMapping("/api")
+@Tag(name = "Time API", description = "API для получения текущего времени")
 public class TimeController {
+
+    private final TimeService timeService;
+
+    @Operation(summary = "Получить текущее время", description = "Метод возвращает текущее время сервера с указанием таймзоны.")
+    @GetMapping("/time")
+    public ResponseEntity<DateTimeDto> getTime() {
+        return ResponseEntity.ok(timeService.getTimeNow());
+    }
+
 }
