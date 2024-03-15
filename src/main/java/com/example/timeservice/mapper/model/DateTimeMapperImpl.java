f689812ca8117319ec408c2d0e98ff0e5fbec772
anton <89879639755a@gmail.com>
2024-03-15 16:05:57 +0300

    реализация маппера времени

--- src/main/java/com/example/timeservice/mapper/model/DateTimeMapperImpl.java
+++ src/main/java/com/example/timeservice/mapper/model/DateTimeMapperImpl.java
@@ -1,12 +1,24 @@
 package com.example.timeservice.mapper.model;
 
-import com.example.timeservice.dto.DateTImeDto;
-import com.example.timeservice.mapper.DateTime;
+import com.example.timeservice.dto.DateTimeDto;
+import com.example.timeservice.mapper.DateTimeMapper;
 
-public class DateTimeImpl implements DateTime {
+import java.time.LocalDateTime;
+import java.time.OffsetDateTime;
+import java.time.ZoneId;
+import java.util.Date;
+
+// реализация интерфейса маппера DateTimeMapper
+public class DateTimeMapperImpl implements DateTimeMapper {
     @Override
-    public DateTImeDto fromDate() {
-
-        return null;
+    public DateTimeDto fromDate(OffsetDateTime currentTime, ZoneId zoneId, String timeString) {
+        int year = currentTime.getYear();
+        int month = currentTime.getMonthValue();
+        int day = currentTime.getDayOfMonth();
+        int hour = currentTime.getHour();
+        int minutes = currentTime.getMinute();
+        int seconds = currentTime.getSecond();
+        Date date = Date.from(currentTime.toInstant());
+        return new DateTimeDto(zoneId.toString(), year, month, day, hour, minutes, seconds, date, timeString);
     }
 }
